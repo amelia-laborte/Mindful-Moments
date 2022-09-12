357ede4481db5d1ebd15d87f190ef5bb3f6ffc01
amelia-laborte <93796860+amelia-laborte@users.noreply.github.com>
2022-09-12 10:10:05 -1000

    fixed homepage route

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import axios from "axios";
-// Get the Home page to display the UI button and link to the create route
 // Create a header component to display the name of the project
 
 const HomePage = () => {
@@ -9,7 +8,9 @@ const HomePage = () => {
     return (
         <div>
             <h2>This is a space where you get to jot down all that you are grateful for.</h2>
-            <button><Link to={`/entry/create`}></Link></button>
+            <Link to={`/create`}>
+                <button>Begin My Journey</button>
+            </Link>
         </div>
     )
     
